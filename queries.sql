06e81e4f27a2d0b25e99e3f9a15d79da5eb75a01
AYUSHYA KUMAR <ayushcom.kumar@gmail.com>
2025-03-02 15:45:11 +0530

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,18 @@
+/*1) Retrieve all patient details from Maharashtra
+2)Count the number of male and female patients
+3)Find the average age of patients
+4)List unique states in the dataset
+5)Find patients with high cholesterol levels (above 200)
+
+6)Find the top 5 states with the highest number of heart attack cases
+7)Find patients who have both diabetes and hypertension
+8)Find patients older than the average age in the dataset
+9)Rank patients based on their cholesterol level within each state
+10)Find the total number of patients in each state and sort them in descending order
+
+11)Find the percentage of patients at high heart attack risk in each state
+12)Identify the top 3 patients in each state with the highest blood pressure (Systolic + Diastolic BP)
+13)Find the state with the highest average emergency response time
+14)Find patients with an annual income higher than the average income of their state
+15)Classify patients into income brackets (Low, Medium, High) based on their Annual Income*/
+
